71599fff6107e51676968eb0fcfa75063e6465bb
Kerim ALTINTOP <kerimaltintop@gmail.com>
2020-12-15 13:14:27 +0300

    Migration Oluşturuldu

--- KerimProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Contexts/ToDoContext.cs
+++ KerimProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Contexts/ToDoContext.cs
@@ -8,7 +8,7 @@ namespace KerimProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Contexts
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=.;database=ToDo;integrated security=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-I8QOE0S\\SQLEXPRESS;database=ToDo;integrated security=true");
         }
 
         public DbSet<Work> Works { get; set; }
