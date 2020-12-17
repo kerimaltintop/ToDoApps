0c2f9c1c05d377ba8d2a73d8936c22029a858061
Kerim ALTINTOP <kerimaltintop@gmail.com>
2020-12-17 16:12:18 +0300

    partial view oluşturuldu

---
+++ KerimProje.ToDo.Web/TagHelpers/Paragraf.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace KerimProje.ToDo.Web.TagHelpers
+{
+    [HtmlTargetElement("kerim")]
+    public class Paragraf : TagHelper
+    {
+        public string getData { get; set; } = "Kerim ALTINTOP";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string data = string.Empty;
+            data = "<p> <b>" + getData + " </b> </p>";
+            output.Content.SetHtmlContent(data);
+            base.Process(context, output);
+        }
+
+    }
+}
