c9222685477a3f97dc3cf0a49e0ad259c80cd8d2
Kerim ALTINTOP <kerimaltintop@gmail.com>
2020-12-17 10:59:33 +0300

    mvc eklendi

--- KerimProje.ToDo.Web/Startup.cs
+++ KerimProje.ToDo.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace KerimProje.ToDo.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //projeye mvc dahil oldu
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,16 @@ namespace KerimProje.ToDo.Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}"
+                    );
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
         }
     }
