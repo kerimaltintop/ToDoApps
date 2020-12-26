5f789a1489ecbc1bef1b57dff5e399934ae59aab
Kerim ALTINTOP <kerimaltintop@gmail.com>
2020-12-27 00:58:15 +0300

    Task Form

--- KerimProje.ToDo.WebUI/Areas/Admin/Controllers/TaskController.cs
+++ KerimProje.ToDo.WebUI/Areas/Admin/Controllers/TaskController.cs
@@ -1,14 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KerimProje.ToDo.Business.Interfaces;
+using KerimProje.ToDo.Entities.Concrete;
+using KerimProje.ToDo.WebUI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
 
 namespace KerimProje.ToDo.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class TaskController : Controller
     {
-        [Area("Admin")]
+
+        private readonly ITaskService _taskService;
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
         public IActionResult Index()
         {
             TempData["Active"] = "task";
-            return View();
+            List<Task> tasks = _taskService.GetAll();
+            List<TaskListViewModel> taskListViewModels = new List<TaskListViewModel>();
+            foreach (var item in tasks)
+            {
+                TaskListViewModel model = new TaskListViewModel
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Explanation = item.Explanation,
+                    Status = item.Status,
+                    CreationDate = item.CreationDate,
+                    UrgencyId = item.UrgencyId,
+                    Urgency = item.Urgency
+                };
+                taskListViewModels.Add(model);
+            }
+            return View(taskListViewModels);
         }
     }
 }
---
+++ KerimProje.ToDo.WebUI/Areas/Admin/Models/TaskListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using KerimProje.ToDo.Entities.Concrete;
+using System;
+
+namespace KerimProje.ToDo.WebUI.Areas.Admin.Models
+{
+    public class TaskListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Explanation { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public int UrgencyId { get; set; }
+        public Urgency Urgency { get; set; }
+    }
+}
